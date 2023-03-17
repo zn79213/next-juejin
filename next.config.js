7dbcb198c958f1667183401064895ce633232542
zn79213 <1762810418@qq.com>
2023-03-17 18:33:27 +0800

    achieve basic function

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    // reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {key: "Access-Control-Allow-Origin", value: "http://127.0.0.1:3000"},
+                    {key: "Access-Control-Allow-Methods", value: "*"},
+                    {key: "Access-Control-Allow-Headers", value: "*"},
+                ]
+            },
 
+        ]
+    },
+    // compiler: {
+    //   styledComponents: true,
+    // },
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       protocol: 'http',
+    //       hostname: '127.0.0.1',
+    //       port: '1337',
+    //       pathname: '/upload/**',
+    //     },
+    //   ],
+    // },
+}
 module.exports = nextConfig
